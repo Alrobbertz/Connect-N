00c3e108db39f087cf5828022bbdb5e2f2a50339
dowdtd16 <34553021+dowdtd16@users.noreply.github.com>
2018-09-03 17:14:12 -0400

    Update Heuristic.java

--- src/Utilities/Heuristic.java
+++ src/Utilities/Heuristic.java
@@ -28,11 +28,12 @@ public class Heuristic {
                         //sets the new max for consecutive pieces
                         if (in_a_row > max) {
                             max = in_a_row;
-
+                            //start postions for column and row
                             startc = column + offset - (max - 1);
                             startr = row;
                         }
                     }
+                    //sets in a row to 0 if there are currently no pieces in a row
                     else
                     {
                         in_a_row = 0;
@@ -44,14 +45,16 @@ public class Heuristic {
                 // Calcuate, based on startr and startc, whether the sequence is open or not
                 // An open sequence is a sequence, that has the possibility to be a sequence of four in a row
 
-                for (int offset = 0; offset <= N - max; offset++) { // TODO Will have to change this for different N
+                for (int offset = 0; offset <= N - max; offset++) { 
+                    //checks to see if it is within the board
                     if (startc - offset + (N-1) >= board.columns) { continue; }
                     if (startc - offset < 0) { break; }
 
                     boolean seq = true;
+                    //checks the columns
                     for (int i = 0; i < N; i++) { // TODO Will have to change this for different N
                         int v = data[row][startc - offset + i];
-
+                        
                         seq = seq && (v == 0 || v == player_turn);
 
                         if (!seq) { break; }
@@ -67,28 +70,33 @@ public class Heuristic {
             }
         }
 
-        // Check vertical
+        // Check vertical pieces in a row
         for (int column = 0; column < board.columns; column++) {
-            for (int row = 0; row < board.rows - N; row++) { // TODO Will have to change this for different N
+            //goes through the rows
+            for (int row = 0; row < board.rows - N; row++) {
+                //sets starting parameters
                 int in_a_row = 0;
                 int max = 0;
                 int startc = -1;
                 int startr = -1;
-
-                for (int offset = 0; offset <= N; offset++) { // TODO Will have to change this for different N
+                //checks through rows with the offset
+                for (int offset = 0; offset <= N; offset++) { 
                     //System.out.println("Check Vertical:" + " row:" + (row + offset) + " col: " + column );
                     int value = data[row + offset][column];
-
+                    //checks if the value pulled from data is the players turn
                     if (value == player_turn)
                     {
+                        //adds one to in a row 
                         in_a_row += 1;
                         if (in_a_row > max) {
+                            //sees if it the new max for items in a row
                             max = in_a_row;
 
                             startc = column;
                             startr = row + offset - (max - 1);
                         }
                     }
+                    //if their is no max sets in a row sets it as 0
                     else
                     {
                         in_a_row = 0;
@@ -97,15 +105,16 @@ public class Heuristic {
 
 
                 boolean isopen = false;
-
-                for (int offset = 0; offset <= N - max; offset++) { // TODO Will have to change this for different N
+                //looks through rows with the offset from 0 to win conditions
+                for (int offset = 0; offset <= N - max; offset++) { 
                     if (startr - offset + (N-1) >= board.rows) { continue; }
                     if (startr - offset < 0) { break; }
-
+    
                     boolean seq = true;
-                    for (int i = 0; i < N; i++) { // TODO Will have to change this for different N
+                    //checks the rows
+                    for (int i = 0; i < N; i++) { 
                         int v = data[startr - offset + i][column];
-
+                        
                         seq = seq && (v == 0 || v == player_turn);
 
                         if (!seq) { break; }
@@ -121,7 +130,7 @@ public class Heuristic {
             }
         }
 
-        // Check diagonal
+        // Check diagonal for pieces in a row works the same as the previous two but now is checking the diagonal
         for (int column =  0; column < board.columns; column++) { // TODO Will have to change this for different N
             //System.out.println("Check Diagonal Inside Column Loop Forwards");
             for (int row = 0; row < board.rows; row++) {
@@ -176,19 +185,21 @@ public class Heuristic {
 
         return hVal;
     }
-
+       //gets the value of each move on the board
     private static int getValue(Board board, int row, int col){
+        //checks to see if it is in the board
         if((row >= 0 && row < board.rows) && (col >= 0 && col < board.columns)){
+            //returns value of postion
             return board.getBoardMatrix()[row][col];
         }
-
+        //error if it is outide the board
         return -1;
     }
 
     public static int deltaheurestic(int max) {
         int h = (int)Math.pow(max, max);
 
-        if (max == 4) { h = (int)Math.pow(h, max); } // TODO Will have to change this for different N
+        if (max == N) { h = (int)Math.pow(h, max); } 
 
         return h;
     }
